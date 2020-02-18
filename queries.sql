4461f375fae26f50ee39619f156e9ac53e53a4a8
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-18 09:48:35 -0500

    stretch

--- queries.sql
+++ queries.sql
@@ -18,7 +18,7 @@ select * from customers where city='London' or city='Madrid' or country='Brazil'
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
-insert into customers (customername, contactname, address, city, postalcode, country) values ('The Shire', 'Bilbo Baggins', '1 Hobbit-Hole', 'Bag End', '111', 'Middle Earth');
+insert into customers (customername, contactname, address, city, postalcode, country) values ('The Shire','Bilbo Baggins','1 Hobbit-Hole', 'Bag End','111','Middle Earth');
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
 
@@ -26,4 +26,8 @@ update customers set postalcode='11122' where customerid='92';
 
 -- (Stretch) Find a query to discover how many different cities are stored in the Customers table. Repeats should not be double counted
 
+select count (distinct city) from customers;
+
 -- (Stretch) Find all suppliers who have names longer than 20 characters. You can use `length(SupplierName)` to get the length of the name
+
+select * from suppliers where length(suppliername) > 20;
