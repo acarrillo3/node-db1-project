dedbde5f3b3ad955fb42b6c3bc9917888dc044af
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2020-02-18 09:46:17 -0500

    crud operations and validation

--- server.js
+++ server.js
@@ -6,4 +6,84 @@ const server = express();
 
 server.use(express.json());
 
+// Create an account
+server.post("/", validateBody, (req, res) => {
+    db('accounts').insert(req.body, 'idz')
+    .then(id => {
+        res.status(201).json({ message: `Successfully added a new account with id of ${id}.` })
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error adding a new account." })
+    })
+})
+
+//Read all accounts
+server.get('/', (req, res) => {
+    db('accounts')
+    .then(accounts => {
+        res.status(200).json(accounts)
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error getting accounts." })
+    })
+})
+
+// Read an individual account
+server.get('/:id', validateId, (req, res) => {
+    res.status(200).json(req.account)
+})
+
+// Update an account 
+server.get('/:id', validateId, validateBody, (req, res) => {
+    db('accounts').where({ id: req.params.id }).update({ name: req.body.name, budget: req.body.budget })
+    .then(count => {
+        res.status(200).json({ message: ` Succesfully updated ${count} account.` })
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error updating the account." })
+    })
+})
+
+//Delete an account
+server.delete('/:id', validateId, (req, res) => {
+    db('accounts').where({ id: req.params.id }).del()
+    .then(count => {
+        res.status(200).json({ message: `Successfully deleted ${count} account.` })
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "There was an error deleting the account." })
+    })
+})
+
+// Middleware functions
+
+//validates ID to make sure it exists
+function validateId(req, res, next) {
+    db.del('accounts').where({id: req.params.id})
+    .first()
+    .then(account => {
+        !account ?
+        res.status(404).json({ message: `No user with id ${req.params.id} exists.` }) :
+        (req.account = account, next())
+    })
+    .catch(err => {
+        res.status(500).json({ errorMessage: "there was an error getting that account." })
+    })
+}
+
+//validates request body for existence, required keys, and a unique name. crazy nested if elses
+function validateBody(req, res, next){
+    Object.keys(req.body).length === 0 && req.body.constructor === Object ?
+    res.status(400).json({ message: "missing request body." }) :
+        !req.body.name || !req.body.budget || req.body.budget.constructor === String ?
+        res.status(400).json({ message: "name and budget are required. name is a string budget is a number" }) :
+        db("accounts").where({ name: req.body.name })
+        .first()
+        .then(account => {
+            !account ? next() : res.status(400).json({ message: "name is currently in use by another account. please change name" })
+        })
+        .catch(err => {
+            res.status(500).json({ message: "couldn't compare that account name to stored name."})
+        })
+}
 module.exports = server;
